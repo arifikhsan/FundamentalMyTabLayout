5298d6c90c020f82fd6e6c974e12c8da64070745
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-25 17:12:10 +0700

    two tab layout

--- app/src/main/java/com/example/fundamentalmytablayout/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalmytablayout/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.fundamentalmytablayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager = findViewById<ViewPager>(R.id.view_pager)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+        viewPager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(viewPager)
+
+        supportActionBar?.elevation = 0f
     }
 }
